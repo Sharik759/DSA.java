f3209af22dc9d01ea6659a357ab654bf92a45402
Sharik759 <sharikmohammed41@gmail.com>
2026-07-26 11:39:23 +0530

    Add files via upload

---
+++ LinearSearch.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+        
+            arr[i] = sc.nextInt();
+        }
+
+        boolean found = true;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == n) {
+                System.out.println("found");
+                found = true;
+                break;
+            }
+        }
+
+       
+    }
